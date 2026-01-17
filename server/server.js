3d5e050f8be0a495bb6c152030e6c200be5d33f4
Gi-Joe <jgacheru504@gmail.com>
2026-01-17 12:54:20 +0300

    initial project setup

---
+++ server/server.js
@@ -0,0 +1,8 @@
+import express from "express"
+
+const app = express()
+const PORT = process.env.PORT || 3000
+app.get('/api', (req, res)=> {
+    res.json({status: 'ok'})
+})
+app.listen(PORT, ()=> console.log(`live on ${PORT}`))
