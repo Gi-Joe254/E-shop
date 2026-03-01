c673ab2dfd2d19a659d2f78c80d8351a5a9cb67e
Gi-Joe <jgacheru504@gmail.com>
2026-03-01 14:58:53 +0300

    changed session secure from false to production

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use(session({
     cookie: {
         maxAge: 60 * 60 * 1000,
         httpOnly: true,
-        secure: false,
+        secure: process.env.NODE_ENV === 'production',
         sameSite: 'lax'
     }
 }))
