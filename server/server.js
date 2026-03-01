07e9ef869766077c6bc191d9b07ba6d461cc6a89
Gi-Joe <jgacheru504@gmail.com>
2026-03-01 15:57:00 +0300

    removed localhost origin in cors to debug cookies not set on admin login

--- server/server.js
+++ server/server.js
@@ -10,8 +10,7 @@ const PORT = process.env.PORT || 3000
 app.use(cors({
   origin: (origin, callback) => {
     if (
-      !origin || // allow server-side tools
-      origin.includes("localhost") ||
+      !origin ||
       origin.includes("vercel.app")
     ) {
       callback(null, true)
