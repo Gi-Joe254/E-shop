6aefcb4c068106d5a3ee1867cef8595f418c07ac
Gi-Joe <jgacheru504@gmail.com>
2026-03-01 15:17:20 +0300

    set session secure to true

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use(session({
     cookie: {
         maxAge: 60 * 60 * 1000,
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: 'true',
         sameSite: 'none'
     }
 }))
